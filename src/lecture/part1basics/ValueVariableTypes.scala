e168987a5ad3f157b809aa33de504ba6d9de9964
nishantpanwar <nishant.panwar18@gmail.com>
2021-06-21 18:49:04 +0530

    ValueVariableTypes file added

--- src/lecture/part1basics/ValueVariableTypes.scala
+++ src/lecture/part1basics/ValueVariableTypes.scala
@@ -8,5 +8,27 @@ object ValueVariableTypes extends App {
   val x: Int = 42
   println(x)
 
+  // VALS ARE IMMUTABLE
+
+  // COMPILER can infer types
+
+  val aString : String = "Hello"
+  val anotherString = "Bye"
+
+  val aBoolean = true
+  val aChar : Char= 'N'
+  val aInt: Int = 18
+  val aShort : Short = 12345
+  val aLong : Long = 323254355365465L
+  val aFloat: Float = 2.0f
+  val aDouble: Double = 3.14
+
+  // variables
+  var aVariable : Int = 4
+
+  aVariable = 12 // Side Effects
+
+
+
 
 }

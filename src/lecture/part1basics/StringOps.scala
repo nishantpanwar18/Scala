bf7966bc4ad112f9c17dedae25e12c9c26058d0b
nishantpanwar18 <nishant.panwar18@gmail.com>
2022-10-30 11:46:39 +0530

    new file

---
+++ src/lecture/part1basics/StringOps.scala
@@ -0,0 +1,8 @@
+package lecture.part1basics
+
+/**
+ * Created by Nishant Panwar on 02-07-2021.
+ */
+object StringOps {
+
+}
---
+++ src/lecture/part2oop/OOBasics.scala
@@ -0,0 +1,10 @@
+package lecture.part2oop
+
+/**
+ * Created by Nishant Panwar on 03-07-2021.
+ */
+object OOBasics extends App {
+
+
+
+}
